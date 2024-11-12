befef3615af9b940ad16019f978f8f339814923a
coha <coha@itu.dk>
2024-11-12 11:12:30 +0100

    fixed small mistake in querys

--- Performace_test/sql/balanced_role_sql.py
+++ Performace_test/sql/balanced_role_sql.py
@@ -8,17 +8,17 @@ def generate_role_queries(db,head,leaf1):
     if db == "Snowflake":
         return [
             f"CREATE OR REPLACE ROLE {leaf1};",
-            f"GRANT ROLE {head} TO ROLE {leaf1};"
+            f"GRANT ROLE {leaf1} TO ROLE {head};"
         ]
     elif db == "PostgreSql":
         return [
             f"CREATE ROLE {leaf1};",
-            f"GRANT {head} TO {leaf1};"
+            f"GRANT {leaf1} TO {head};"
         ]
     else:
         # MariaDB
         return [
             f"CREATE ROLE {leaf1};",
-            f"GRANT {head} TO {leaf1};"
+            f"GRANT {leaf1} TO {head};"
         ]
 
