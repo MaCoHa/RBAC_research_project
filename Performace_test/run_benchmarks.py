08c80bf5a1c098a9ca8b88b3cfd0e19a4401dea0
Mads <klintevej15@gmail.com>
2025-03-07 14:05:27 +0100

    change the run script to fit the new tests

--- Performace_test/run_benchmarks.py
+++ Performace_test/run_benchmarks.py
@@ -6,23 +6,34 @@ import Select_tests.select_star as select
 repetitions = 2
 time_limit_minutes = 15
 
-### have been run
+### Snowflake
 
-#wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_snowflake_stats",db="Snowflake")
+### ************** have runn ************************
+wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_snowflake_stats",db="Snowflake")
+
+### ************** needs to runn ************************
 #deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_snowflake_stats",db="Snowflake")
 #balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_snowflake_stats",db="Snowflake")
 
-### yet to be run
+#select.main(file_name="benchmark_select_star_Snowflake_Wide_tree",database="Snowflake",tree_type="Wide_tree",time_limit_minutes = time_limit_minutes)
+#select.main(file_name="benchmark_select_star_Snowflake_Deep_tree",database="Snowflake",tree_type="Deep_tree",time_limit_minutes = time_limit_minutes)
+#select.main(file_name="benchmark_select_star_Snowflake_Balanced_tree",database="Snowflake",tree_type="Balanced_tree",time_limit_minutes = time_limit_minutes)
+
+
+
+### PostgreSQL
 
 #wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_postgresql_stats",db="PostgreSql")
 #deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_postgresql_stats",db="PostgreSql")
 #balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_postgresql_stats",db="PostgreSql")
+
+### MariaDB
 
 #wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_mariadb_stats",db="MariaDB")
 #deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_mariadb_stats",db="MariaDB")
 #balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_mariadb_stats",db="MariaDB")
 
 
-#select.main(file_name="benchmark_select_star_role_1000_stats")
+
 
 
