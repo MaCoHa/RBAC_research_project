7e79cbd3342ae075141b666ae88e90df5c5cafdf
Mads <klintevej15@gmail.com>
2025-03-15 13:10:53 +0100

    #Change, Made a change to how experiments were run

--- Performace_test/run_benchmarks.py
+++ Performace_test/run_benchmarks.py
@@ -2,36 +2,100 @@ import create_wide_role_tree as wide
 import create_deep_role_tree as deep
 import create_balanced_role_tree as balanced
 import Select_tests.select_star as select
+import connection_testing as testing
 # starndard are rep: 2. time: 15
 repetitions = 2
 time_limit_minutes = 15
 
-### Snowflake
 
+
+
+
+
+experiments = [
+    # Testing
+    #("cornnection test", lambda: testing.main()),
+
+    # Already Run
+
+    #("Snowflake wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_snowflake_stats", db="Snowflake")),
+    #("Snowflake deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_snowflake_stats", db="Snowflake")),
+    #("Snowflake balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_snowflake_stats", db="Snowflake")),
+    #("Snowflake wide_tree Select Test", lambda: select.main(file_name="benchmark_select_star_Snowflake_Wide_tree", database="Snowflake", tree_type="Wide_tree", time_limit_minutes=time_limit_minutes)),
+    
+    #("Snowflake deep_tree Select Test", lambda: select.main(file_name="benchmark_select_star_Snowflake_Deep_tree", database="Snowflake", tree_type="Deep_tree", time_limit_minutes=time_limit_minutes)),
+    
+   
+    #("Snowflake balanced_tree Select Test", lambda: select.main(file_name="benchmark_select_star_Snowflake_Balanced_tree", database="Snowflake", tree_type="Balanced_tree", time_limit_minutes=time_limit_minutes)),
+    # Running
+    # Needs to Run    
+
+    # Not Ready
+
+   # ("PostgreSQL wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_postgresql_stats", db="PostgreSql")),
+    #("PostgreSQL deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_postgresql_stats", db="PostgreSql")),
+    #("PostgreSQL balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_postgresql_stats", db="PostgreSql")),
+   
+    #("PostgreSQL wide_tree Select Test", lambda: select.main(file_name="benchmark_select_star_PostgreSql_Wide_tree", database="PostgreSql", tree_type="Wide_tree", time_limit_minutes=time_limit_minutes)),
+    #("PostgreSQL deep_tree Select Test", lambda: select.main(file_name="benchmark_select_star_PostgreSql_Deep_tree", database="PostgreSql", tree_type="Deep_tree", time_limit_minutes=time_limit_minutes)),
+    #("PostgreSQL balanced_tree Select Test", lambda: select.main(file_name="benchmark_select_star_PostgreSql_Balanced_tree", database="PostgreSql", tree_type="Balanced_tree", time_limit_minutes=time_limit_minutes)),
+
+    #("MariaDB wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_mariadb_stats", db="MariaDB")),
+    #("MariaDB deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_mariadb_stats", db="MariaDB")),
+    ("MariaDB balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_mariadb_stats", db="MariaDB")),
+   
+    #("MariaDB wide_tree Select Test", lambda: select.main(file_name="benchmark_select_star_MariaDB_Wide_tree", database="MariaDB", tree_type="Wide_tree", time_limit_minutes=time_limit_minutes)),
+    #("MariaDB deep_tree Select Test", lambda: select.main(file_name="benchmark_select_star_MariaDB_Deep_tree", database="MariaDB", tree_type="Deep_tree", time_limit_minutes=time_limit_minutes)),
+    #("MariaDB balanced_tree Select Test", lambda: select.main(file_name="benchmark_select_star_MariaDB_Balanced_tree", database="MariaDB", tree_type="Balanced_tree", time_limit_minutes=time_limit_minutes)),
+]
+
+
+for index, (title, experiment) in enumerate(experiments, start=1):
+
+    print(f"Running {title} experiment {index} out of {len(experiments)}\n", end="\r")
+    experiment()  # Calls the function
+
+print(f"******** all {len(experiments)} have run ********")
+
+"""
 ### ************** have runn ************************
 wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_snowflake_stats",db="Snowflake")
+deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_snowflake_stats",db="Snowflake")
 
 ### ************** needs to runn ************************
-#deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_snowflake_stats",db="Snowflake")
-#balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_snowflake_stats",db="Snowflake")
 
-#select.main(file_name="benchmark_select_star_Snowflake_Wide_tree",database="Snowflake",tree_type="Wide_tree",time_limit_minutes = time_limit_minutes)
-#select.main(file_name="benchmark_select_star_Snowflake_Deep_tree",database="Snowflake",tree_type="Deep_tree",time_limit_minutes = time_limit_minutes)
-#select.main(file_name="benchmark_select_star_Snowflake_Balanced_tree",database="Snowflake",tree_type="Balanced_tree",time_limit_minutes = time_limit_minutes)
+balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_snowflake_stats",db="Snowflake")
+
+select.main(file_name="benchmark_select_star_Snowflake_Wide_tree",database="Snowflake",tree_type="Wide_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_Snowflake_Deep_tree",database="Snowflake",tree_type="Deep_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_Snowflake_Balanced_tree",database="Snowflake",tree_type="Balanced_tree",time_limit_minutes = time_limit_minutes)
 
 
 
+            
 ### PostgreSQL
 
-#wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_postgresql_stats",db="PostgreSql")
-#deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_postgresql_stats",db="PostgreSql")
-#balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_postgresql_stats",db="PostgreSql")
+wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_postgresql_stats",db="PostgreSql")
+deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_postgresql_stats",db="PostgreSql")
+balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_postgresql_stats",db="PostgreSql")
+
+select.main(file_name="benchmark_select_star_PostgreSql_Wide_tree",database="PostgreSql",tree_type="Wide_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_PostgreSql_Deep_tree",database="PostgreSql",tree_type="Deep_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_PostgreSql_Balanced_tree",database="PostgreSql",tree_type="Balanced_tree",time_limit_minutes = time_limit_minutes)
+
+
 
 ### MariaDB
 
-#wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_mariadb_stats",db="MariaDB")
-#deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_mariadb_stats",db="MariaDB")
-#balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_mariadb_stats",db="MariaDB")
+wide.main(repetitions,time_limit_minutes,file_name="benchmark_wide_tree_mariadb_stats",db="MariaDB")
+deep.main(repetitions,time_limit_minutes,file_name="benchmark_deep_tree_mariadb_stats",db="MariaDB")
+balanced.main(repetitions,time_limit_minutes,file_name="benchmark_balanced_tree_mariadb_stats",db="MariaDB")
+
+select.main(file_name="benchmark_select_star_MariaDB_Wide_tree",database="MariaDB",tree_type="Wide_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_MariaDB_Deep_tree",database="MariaDB",tree_type="Deep_tree",time_limit_minutes = time_limit_minutes)
+select.main(file_name="benchmark_select_star_MariaDB_Balanced_tree",database="MariaDB",tree_type="Balanced_tree",time_limit_minutes = time_limit_minutes)
+
+"""
 
 
 
