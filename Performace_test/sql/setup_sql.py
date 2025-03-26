a3660304211e5e8f6b704ce92b784a5ce250b40e
MaCoHa <klintevej15@gmail.com>
2025-03-26 15:15:38 +0100

    #bugfix

--- Performace_test/sql/setup_sql.py
+++ Performace_test/sql/setup_sql.py
@@ -27,13 +27,13 @@ def generate_setup_queries(db):
 
             "GRANT Role0 TO 'connection'@'%';",
 
-            "use mariadb;",
-            """CREATE OR REPLACE TABLE FOO (    
+
+            """CREATE TABLE mariadb.FOO (    
             website_name VARCHAR(25) NOT NULL,    
             server_name VARCHAR(20),    
             creation_date DATE);""",
 
-            """INSERT INTO FOO (website_name, server_name, creation_date)
-            VALUES ('example.com', 'server01', '2024-11-06');""",
+            """INSERT INTO mariadb.FOO (website_name, server_name, creation_date)
+            VALUES ('example.com', 'server01', '2024-11-06');"""
         ]
 
