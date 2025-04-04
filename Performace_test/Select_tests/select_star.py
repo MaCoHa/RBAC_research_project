e5c90f5215bce7fb451afcbab16a85c8431000b1
MaCoHa <klintevej15@gmail.com>
2025-04-04 19:59:38 +0200

    #

--- Performace_test/Select_tests/select_star.py
+++ Performace_test/Select_tests/select_star.py
@@ -14,10 +14,10 @@ import Select_tests.create_trees as create
 
 table = "foo"
 ### True sizes
-tree_sizes = [1000,10_000,100_000]
+#tree_sizes = [1000,10_000,100_000]
 
 ### Test sizes 
-#tree_sizes = [1,10]
+tree_sizes = [1,10]
 
 def main(file_name,database,tree_type,time_limit_minutes,repetitions):
     
@@ -76,6 +76,8 @@ def main(file_name,database,tree_type,time_limit_minutes,repetitions):
                 sys.exit(1)
             cur = conn.cursor()
 
+        util.remove_roles(database,cur,1000+1)
+        return
         
         try:
             #print(f'Create tree {tree_type} on db : {database}') 
