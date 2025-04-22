377fd6f853dcedccc1eda383f0fe0a068272fb4f
Mads <klintevej15@gmail.com>
2025-04-22 11:22:51 +0200

    ec2 run

--- Performace_test/run_benchmarks.py
+++ Performace_test/run_benchmarks.py
@@ -4,11 +4,11 @@ import create_balanced_role_tree as balanced
 import Select_tests.select_star as select
 import connection_testing as testing
 import setup as setup
-import benchmark.create_processed.plot as plot
+#import benchmark.create_processed.plot as plot
 # starndard are rep: 2. time: 15
 select_repetitions = 5
-repetitions = 2
-time_limit_minutes = 15
+repetitions = 1
+time_limit_minutes = 5
 
 
 
@@ -40,7 +40,7 @@ experiments = [
     #("Snowflake_EC2 balanced_tree Select Test", lambda: select.main(file_name="benchmark_select_star_Snowflake_EC2_Balanced_tree", database="Snowflake_EC2", tree_type="Balanced_tree", time_limit_minutes=time_limit_minutes,repetitions=select_repetitions)),
 
 
-    ("PostgreSQL wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_postgresql_stats_error", db="PostgreSql")),
+    #("PostgreSQL wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_postgresql_stats_error", db="PostgreSql")),
     #("PostgreSQL deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_postgresql_stats_error", db="PostgreSql")),
     #("PostgreSQL balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_postgresql_stats_error", db="PostgreSql")),
    
@@ -57,11 +57,11 @@ experiments = [
     #("MariaDB balanced_tree Select Test", lambda: select.main(file_name="benchmark_select_star_MariaDB_Balanced_tree", database="MariaDB", tree_type="Balanced_tree", time_limit_minutes=time_limit_minutes,repetitions=select_repetitions)),
 
 
-    #("PostgreSQL EC2 Setup the database", lambda: setup.main(db="PostgreSql_EC2")),
+    ("PostgreSQL EC2 Setup the database", lambda: setup.main(db="PostgreSql_EC2")),
 
-    #("PostgreSQL EC2 wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
-    #("PostgreSQL EC2 deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
-    #("PostgreSQL EC2 balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
+    ("PostgreSQL EC2 wide_tree Create Test", lambda: wide.main(repetitions, time_limit_minutes, file_name="benchmark_wide_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
+    ("PostgreSQL EC2 deep_tree Create Test", lambda: deep.main(repetitions, time_limit_minutes, file_name="benchmark_deep_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
+    ("PostgreSQL EC2 balanced_tree Create Test", lambda: balanced.main(repetitions, time_limit_minutes, file_name="benchmark_balanced_tree_postgresql_ec2_stats", db="PostgreSql_EC2")),
    
     #("PostgreSQL EC2 wide_tree Select Test", lambda: select.main(file_name="benchmark_select_star_postgresql_ec2_Wide_tree", database="PostgreSql_EC2", tree_type="Wide_tree", time_limit_minutes=time_limit_minutes,repetitions=select_repetitions)),
     #("PostgreSQL EC2 deep_tree Select Test", lambda: select.main(file_name="benchmark_select_star_postgresql_ec2_Deep_tree", database="PostgreSql_EC2", tree_type="Deep_tree", time_limit_minutes=time_limit_minutes,repetitions=select_repetitions)),
@@ -83,12 +83,12 @@ experiments = [
 
 
 
-    ("Create Snowflake data graphs", lambda: plot.plot_line_graph("Snowflake median grant and create Role over 15 min","Snowflake_grant_data.csv","Snowflake_grant_data")),
-    ("Create PostgreSql data graphs", lambda: plot.plot_line_graph("PostgreSql median grant and create Role over 15 min","PostgreSql_grant_data.csv","PostgreSql_grant_data")),
-    ("Create MariaDb data graphs", lambda: plot.plot_line_graph("MariaDb median grant and create Role over 15 min","MariaDb_grant_data.csv","MariaDb_grant_data")),
-    ("Create Snowflake_EC2 data graphs", lambda: plot.plot_line_graph("Snowflake_EC2 median grant and create Role over 15 min","Snowflake_EC2_grant_data.csv","Snowflake_EC2_grant_data")),
-    ("Create PostgreSql_EC2 data graphs", lambda: plot.plot_line_graph("PostgreSql_EC2 median grant and create Role over 15 min","PostgreSql_EC2_grant_data.csv","PostgreSql_EC2_grant_data")),
-    ("Create MariaDb_EC2 data graphs", lambda: plot.plot_line_graph("MariaDb_EC2 median grant and create Role over 15 min","MariaDb_EC2_grant_data.csv","MariaDb_EC2_grant_data"))
+    #("Create Snowflake data graphs", lambda: plot.plot_line_graph("Snowflake median grant and create Role over 15 min","Snowflake_grant_data.csv","Snowflake_grant_data")),
+    #("Create PostgreSql data graphs", lambda: plot.plot_line_graph("PostgreSql median grant and create Role over 15 min","PostgreSql_grant_data.csv","PostgreSql_grant_data")),
+    #("Create MariaDb data graphs", lambda: plot.plot_line_graph("MariaDb median grant and create Role over 15 min","MariaDb_grant_data.csv","MariaDb_grant_data")),
+    #("Create Snowflake_EC2 data graphs", lambda: plot.plot_line_graph("Snowflake_EC2 median grant and create Role over 15 min","Snowflake_EC2_grant_data.csv","Snowflake_EC2_grant_data")),
+    #("Create PostgreSql_EC2 data graphs", lambda: plot.plot_line_graph("PostgreSql_EC2 median grant and create Role over 15 min","PostgreSql_EC2_grant_data.csv","PostgreSql_EC2_grant_data")),
+    #("Create MariaDb_EC2 data graphs", lambda: plot.plot_line_graph("MariaDb_EC2 median grant and create Role over 15 min","MariaDb_EC2_grant_data.csv","MariaDb_EC2_grant_data"))
     
 
 
